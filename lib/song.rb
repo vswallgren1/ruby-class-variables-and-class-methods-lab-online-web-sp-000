e9d75a50b668e6fc1585e74dd61c8ee91acafdc8
Vince W <vincewallgren@me.com>
2021-03-29 04:51:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,19 @@ class Song
   @@name = 0
   @@artist = 0
   @@genre = 0
+  @@genre_count = 0
+  @@artist_count = 0
 
   attr_accessor :name, :artist, :genre
+
+  def initialize
+    @@genre_count += 1
+    @@artist_count += 1
+  end
+
+  def self.count
+    @@genre_count
+    @@artist_count
+  end
 
 end
