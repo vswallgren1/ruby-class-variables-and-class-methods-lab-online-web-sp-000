23bd5a5b3d8b60be2d5eec034c27658353786f20
Vince W <vincewallgren@me.com>
2021-03-29 05:08:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,9 +6,9 @@ class Song
   @@genres = []
 
   def initialize(name, artist, genre)
-    @@name = name
-    @@artist = artist
-    @@genre = genre
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count = += 1
     @@genres << genre
     @@artists << artist
